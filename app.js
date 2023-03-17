fdd5b7e1473b5481284b3e611564c8c596e6bb17
ShubhamSatyabola <123935629+ShubhamSatyabola@users.noreply.github.com>
2023-03-17 18:21:55 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
+const { TIMEOUT } = require('dns')
 const http = require('http')
 const port = 4000
 
-const server = http.createServer((req,res)=>{
-    res.write('my name is shubham')
-    res.end()
+const server = http.createServer((req, res)=>{
+      
+    // console.log(req.url, req.method, req.headers) 
+    const url = req.url
+    if(url == '/home'){
+        res.write('<html>')
+        res.write('<head><title>my responses</title></head>')   
+        res.write('<body><p>welcome home</p></body>') 
+        res.write('</html>')
+    }
+    else if(url == '/about'){
+        res.write('<html>')
+        res.write('<head><title>my responses</title></head>')   
+        res.write('<body><p>welcome to about us page</p></body>') 
+        res.write('</html>')
+    }    
+    else{
+            res.write('<html>')
+            res.write('<head><title>my responses</title></head>')   
+            res.write('<body><p>welcome to node js project</p></body>') 
+            res.write('</html>')
+        } 
+    res.end()         
+                   
     
 })
 server.listen(port)
